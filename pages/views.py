575ba168b141f1b7db6146bc3632e8211b68be50
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-11-17 19:29:05 +0000

    templates

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,12 @@ from django.http import HttpResponse #(lets u use python in django)
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs): #python lets u pass any arguements and key arguements
-    return HttpResponse ("<h1>Hello world</h1>") #string of html code
+def home_view(request, *args, **kwargs): #python lets u pass any arguements and key arguements, its requesting a url for something(which is here html content)
+    #return HttpResponse ("<h1>Hello world</h1>") #string of html code
+    return render(request, "home.html", {}) # to render the above and for it create a template folder
+
+def contact_view(request, *args, **kwargs): #python lets u pass any arguements and key arguements
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs): #python lets u pass any arguements and key arguements
+    return render(request, "about.html", {})
