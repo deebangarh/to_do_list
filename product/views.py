f42d6f7930e22d562d0f437930f535ae67dfd800
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-11-18 21:04:13 +0000

    objects

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def product_detail_view(request): #keep the functions lower case and diff from the class
+    obj = Product.objects.get(id=2)
+      #  context ={
+      #      'title' : obj.title,
+      #      'description' : obj.description
+       # }
+    context = {
+       'object' : obj
+       }
+    return render(request, "product/detail.html", context)
