5ca9c833b1b5a965d2d3ac63a665553081b7d076
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-11-18 19:49:13 +0000

    template and filter

--- pages/views.py
+++ pages/views.py
@@ -8,9 +8,10 @@ def home_view(request, *args, **kwargs): #python lets u pass any arguements and 
 
 def contact_view(request, *args, **kwargs): #python lets u pass any arguements and key arguements
     my_context = {
-        "my_name" : "Deepika",
+        "title" : "hello, I am coding",
         "my_city" : 30,
-        "my_list" : [1,2,34,43443]
+        "my_list" : [1,2,34,43443],
+        "my_html" :"<h1>hello world</h1>"
             }
     return render(request, "contact.html", my_context)
     #the keys eg my_name becomes the template variables to be used in the html files
