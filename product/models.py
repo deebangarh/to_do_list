1eed2e35d7e1df5a0ee7a520add5b1942c4e6fc5
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-11-19 18:28:18 +0000

    forms

--- product/models.py
+++ product/models.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    featured = models.BooleanField() #null = true, default = true
+    featured = models.BooleanField(default = False) #null = true, default = true
--- product/views.py
+++ product/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+
+from .forms import ProductForm
+
 from .models import Product
+
+def product_create_view(request): #keep the functions lower case and diff from the class
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        # Save the form data to the database
+        form.save()
+        # Create a new instance of the ProductForm to clear the form
+        form = ProductForm()
+
+    context = {'form' : form }
+
+    return render(request, "products/product_create.html", context)
+
+
 # Create your views here.
 def product_detail_view(request): #keep the functions lower case and diff from the class
     obj = Product.objects.get(id=2)
@@ -10,4 +28,4 @@ def product_detail_view(request): #keep the functions lower case and diff from t
     context = {
        'object' : obj
        }
-    return render(request, "product/detail.html", context)
+    return render(request, "products/product_detail.html", context)
