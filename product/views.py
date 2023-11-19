4a285723d4410f5d05452bb1b6b7163890bc468d
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-11-19 19:39:44 +0000

    getandpostrequest

--- product/views.py
+++ product/views.py
@@ -3,19 +3,30 @@ from django.shortcuts import render
 from .forms import ProductForm
 
 from .models import Product
-
 def product_create_view(request): #keep the functions lower case and diff from the class
-    form = ProductForm(request.POST or None)
+   #print(request.GET)
+   #print(request.POST)
+   if request.method == "POST":
+    my_new_title = request.POST.get('title')
+   print(my_new_title)
+   #Product.objects.create(title=my_new_title)
+   context = {}
 
-    if form.is_valid():
-        # Save the form data to the database
-        form.save()
-        # Create a new instance of the ProductForm to clear the form
-        form = ProductForm()
+   return render(request, "products/product_create.html", context)
 
-    context = {'form' : form }
 
-    return render(request, "products/product_create.html", context)
+# def product_create_view(request): #keep the functions lower case and diff from the class
+#     form = ProductForm(request.POST or None)
+
+#     if form.is_valid():
+#         # Save the form data to the database
+#         form.save()
+#         # Create a new instance of the ProductForm to clear the form
+#         form = ProductForm()
+
+#     context = {'form' : form }
+
+#     return render(request, "products/product_create.html", context)
 
 
 # Create your views here.
