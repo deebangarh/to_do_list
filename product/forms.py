aa569a0efa7d050dbd2fab48154957104b9466be
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-11-20 21:42:19 +0000

    validations

--- product/forms.py
+++ product/forms.py
@@ -3,6 +3,21 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='',
+                            widget=forms.TextInput(attrs={
+                               "placeholder": "Your title"
+                            }))
+    description = forms.CharField(
+    required=False,
+    widget=forms.Textarea(
+        attrs={
+            "placeholder": "Your description",
+            "class": "new-class-name two",
+            "id": "my-id-for-textarea",
+            "rows": 20,
+            "columns": 50
+        }
+    ))
     class Meta:
         model = Product
         fields =[
@@ -10,6 +25,14 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "CFE" in title:
+            
+            return title
+        else:
+            raise forms.ValidationError("This is not valid title")
 
 #standard django form
 class RawProductForm(forms.Form):
--- product/views.py
+++ product/views.py
@@ -4,20 +4,20 @@ from .forms import ProductForm, RawProductForm #(to render the django form)
 
 from .models import Product
 
-def product_create_view(request): #keep the functions lower case and diff from the class
- my_form = RawProductForm(request.GET)
- if request.method == "POST":
-   my_form = RawProductForm(request.POST)
- if my_form.is_valid():
-    print (my_form.cleaned_data)
-    Product.objects.create(**my_form.cleaned_data) #**to pass it as an arguement
- else:
-     print (my_form.errors)
- context = {
-       "form" : my_form
-   }
+# def product_create_view(request): #keep the functions lower case and diff from the class
+#  my_form = RawProductForm(request.GET)
+#  if request.method == "POST":
+#    my_form = RawProductForm(request.POST)
+#  if my_form.is_valid():
+#     print (my_form.cleaned_data)
+#     Product.objects.create(**my_form.cleaned_data) #**to pass it as an arguement
+#  else:
+#      print (my_form.errors)
+#  context = {
+#        "form" : my_form
+#    }
 
- return render(request, "products/product_create.html", context)
+#  return render(request, "products/product_create.html", context)
 
 # def product_create_view(request): #keep the functions lower case and diff from the class
 #    #print(request.GET)
@@ -31,21 +31,21 @@ def product_create_view(request): #keep the functions lower case and diff from t
 #    return render(request, "products/product_create.html", context)
 
 
-# def product_create_view(request): #keep the functions lower case and diff from the class
-#     form = ProductForm(request.POST or None)
+def product_create_view(request): #keep the functions lower case and diff from the class
+    form = ProductForm(request.POST or None)
 
-#     if form.is_valid():
-#         # Save the form data to the database
-#         form.save()
-#         # Create a new instance of the ProductForm to clear the form
-#         form = ProductForm()
+    if form.is_valid():
+        # Save the form data to the database
+        form.save()
+        # Create a new instance of the ProductForm to clear the form
+        form = ProductForm()
 
-#     context = {'form' : form }
+    context = {'form' : form }
 
-#     return render(request, "products/product_create.html", context)
+    return render(request, "products/product_create.html", context)
 
 
-# Create your views here.
+# Create your views here.2
 def product_detail_view(request): #keep the functions lower case and diff from the class
     obj = Product.objects.get(id=2)
       #  context ={
